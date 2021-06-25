32eb93f5e74099456b806d0780d13636471158cb
athomas9195 <anna.tina.thomas@gmail.com>
2021-06-25 12:59:54 -0700

    grid details view

---
+++ Flix/VIew Controllers/GridDetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  GridDetailsViewController.h
+//  Flix
+//
+//  Created by Anna Thomas on 6/25/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GridDetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
